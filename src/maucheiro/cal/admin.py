10e53847d6f7b34d393a754b878b7ba024b4d67c
Peter Bouda <pbouda@cidles.eu>
2012-11-06 17:05:21 +0000

    Activated OSM admin.

--- src/maucheiro/cal/admin.py
+++ src/maucheiro/cal/admin.py
@@ -1,4 +1,9 @@
 from django.contrib.gis import admin
 from models import Smell
 
-admin.site.register(Smell, admin.GeoModelAdmin)
+class MyGeoAdmin(admin.options.OSMGeoAdmin):
+    default_lon = -964519
+    default_lat = 4788370
+    default_zoom = 14
+
+admin.site.register(Smell, MyGeoAdmin)
--- src/maucheiro/cal/models.py
+++ src/maucheiro/cal/models.py
@@ -5,7 +5,10 @@ from django.contrib.gis.db import models
 class Smell(models.Model):
     date_start = models.DateTimeField("Start date of smell")
     date_end = models.DateTimeField("End date of smell")
+
     geometry = models.PointField("Location of smell", srid=4326)
 
+    objects = models.GeoManager()
+
     def __unicode__(self):
-        return '%s %s %s' % (self.name, self.geometry.x, self.geometry.y)
+        return '%s - %s: %s %s' % (self.date_start, self.date_end, self.geometry.x, self.geometry.y)
